fee32e19375523f37f2650992570bb59e4dd815e
Marcelo Leopoldino <m.leopoldino@gmail.com>
2021-12-30 11:32:50 -0300

    Adding pagination in the Lancamento return.

--- src/main/java/com/mls/mlsmoneyapi/repository/lancamento/LancamentoRepositoryImpl.java
+++ src/main/java/com/mls/mlsmoneyapi/repository/lancamento/LancamentoRepositoryImpl.java
@@ -3,6 +3,9 @@ package com.mls.mlsmoneyapi.repository.lancamento;
 import com.mls.mlsmoneyapi.model.Lancamento;
 import com.mls.mlsmoneyapi.model.Lancamento_;
 import com.mls.mlsmoneyapi.repository.filter.LancamentoFilter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
@@ -21,7 +24,7 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery{
     private EntityManager manager;
 
     @Override
-    public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter) {
+    public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable) {
         CriteriaBuilder builder = manager.getCriteriaBuilder();
         CriteriaQuery<Lancamento> criteria = builder.createQuery(Lancamento.class);
 
@@ -32,7 +35,9 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery{
         criteria.where(predicates);
 
         TypedQuery<Lancamento> query = manager.createQuery(criteria);
-        return query.getResultList();
+        adicionarRestricoesDePaginacao(query, pageable);
+        
+        return new PageImpl<>(query.getResultList(), pageable, total(lancamentoFilter));
     }
 
     private Predicate[] criarRestricoes(LancamentoFilter lancamentoFilter, CriteriaBuilder builder, Root<Lancamento> root) {
@@ -54,5 +59,28 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery{
         }
 
         return predicates.toArray(new Predicate[predicates.size()]);
+    }
+
+    private void adicionarRestricoesDePaginacao(TypedQuery<Lancamento> query, Pageable pageable) {
+        int paginaAtual = pageable.getPageNumber();
+        int totsalRegistrosPorPagina = pageable.getPageSize();
+        int primeiroRegistroDaPagina = paginaAtual * totsalRegistrosPorPagina;
+
+        query.setFirstResult(primeiroRegistroDaPagina);
+        query.setMaxResults(totsalRegistrosPorPagina);
+
+    }
+
+    private Long total(LancamentoFilter lancamentoFilter) {
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Long> criteria = builder.createQuery(Long.class);
+        Root<Lancamento> root = criteria.from(Lancamento.class);
+
+        Predicate[] predicates = criarRestricoes(lancamentoFilter, builder, root);
+        criteria.where(predicates);
+
+        criteria.select(builder.count(root));
+
+        return manager.createQuery(criteria).getSingleResult();
     }
 }
--- src/main/java/com/mls/mlsmoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/mls/mlsmoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
@@ -2,10 +2,10 @@ package com.mls.mlsmoneyapi.repository.lancamento;
 
 import com.mls.mlsmoneyapi.model.Lancamento;
 import com.mls.mlsmoneyapi.repository.filter.LancamentoFilter;
-
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface LancamentoRepositoryQuery {
 
-    public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+    public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 }
